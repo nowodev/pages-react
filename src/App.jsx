c8fad6a53a45c4cac454d19c73f538107f00e98c
Toluwani Okunowo <43044740+nowodev@users.noreply.github.com>
2025-03-11 15:57:27 -0700

    Setup navigation and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Outlet } from "react-router";
+import Header from "./Header";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div className="min-h-full">
+        <Header />
 
-export default App
+        <main className="-mt-32">
+          <div className="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
+            <div className="rounded-lg bg-white px-5 py-6 shadow-sm sm:px-6">
+              <Outlet />
+            </div>
+          </div>
+        </main>
+      </div>
+    </>
+  );
+}
---
+++ src/Header.jsx
@@ -0,0 +1,137 @@
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { NavLink } from "react-router";
+
+const navigation = [
+  { name: "Frontend Mentor", href: "#" },
+  { name: "Team", href: "#" },
+  { name: "Projects", href: "#" },
+  { name: "Calendar", href: "#" },
+  { name: "Reports", href: "#" },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function Header() {
+  return (
+    <header className="bg-gray-800 pb-32">
+      <Disclosure as="nav" className="bg-gray-800">
+        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+          <div className="border-b border-gray-700">
+            <div className="flex h-16 items-center justify-between px-4 sm:px-0">
+              <div className="flex items-center">
+                <div className="shrink-0">
+                  <img
+                    alt="Your Company"
+                    src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+                    className="size-8"
+                  />
+                </div>
+                <div className="hidden md:block">
+                  <div className="ml-10 flex items-baseline space-x-4">
+                    {navigation.map((item) => (
+                      <NavLink
+                        to={item.href}
+                        className={({ isActive }) =>
+                          classNames(
+                            isActive
+                              ? "bg-gray-900 text-white"
+                              : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                            "rounded-md px-3 py-2 text-sm font-medium"
+                          )
+                        }
+                      >
+                        {item.name}
+                      </NavLink>
+                    ))}
+                  </div>
+                </div>
+              </div>
+
+              <div className="-mr-2 flex md:hidden">
+                {/* Mobile menu button */}
+                <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  <Bars3Icon
+                    aria-hidden="true"
+                    className="block size-6 group-data-open:hidden"
+                  />
+                  <XMarkIcon
+                    aria-hidden="true"
+                    className="hidden size-6 group-data-open:block"
+                  />
+                </DisclosureButton>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <DisclosurePanel className="border-b border-gray-700 md:hidden">
+          <div className="space-y-1 px-2 py-3 sm:px-3">
+            {navigation.map((item) => (
+              <NavLink
+                to={item.href}
+                className={({ isActive }) =>
+                  classNames(
+                    isActive
+                      ? "bg-gray-900 text-white"
+                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                    "block rounded-md px-3 py-2 text-base font-medium"
+                  )
+                }
+              >
+                {item.name}
+              </NavLink>
+            ))}
+          </div>
+          {/* <div className="border-t border-gray-700 pt-4 pb-3">
+            <div className="flex items-center px-5">
+              <div className="shrink-0">
+                <img
+                  alt=""
+                  src={user.imageUrl}
+                  className="size-10 rounded-full"
+                />
+              </div>
+              <div className="ml-3">
+                <div className="text-base/5 font-medium text-white">
+                  {user.name}
+                </div>
+                <div className="text-sm font-medium text-gray-400">
+                  {user.email}
+                </div>
+              </div>
+              <button
+                type="button"
+                className="relative ml-auto shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden"
+              >
+                <span className="absolute -inset-1.5" />
+                <span className="sr-only">View notifications</span>
+                <BellIcon aria-hidden="true" className="size-6" />
+              </button>
+            </div>
+            <div className="mt-3 space-y-1 px-2">
+              {userNavigation.map((item) => (
+                <DisclosureButton
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+                >
+                  {item.name}
+                </DisclosureButton>
+              ))}
+            </div>
+          </div> */}
+        </DisclosurePanel>
+      </Disclosure>
+    </header>
+  );
+}
