8629e45844c8bd2b5584055a1097db88c7df09b1
Toluwani Okunowo <43044740+nowodev@users.noreply.github.com>
2025-04-04 00:55:01 -0700

    Complete Leaderboard design

--- src/React Projects/CardinalHire/CardinalLeaderboard.jsx
+++ src/React Projects/CardinalHire/CardinalLeaderboard.jsx
@@ -1,5 +1,4 @@
-import clsx from "clsx";
-import board from "./board.svg";
+import party from "./party.png";
 
 const people = [
   {
@@ -35,56 +34,32 @@ const people = [
 export default function CardinalLeaderboard() {
   return (
     <div className="container min-h-screen px-4 py-10 mx-auto sm:px-6 lg:px-8 space-y-9">
-      <section className="flex items-center justify-between px-6 py-3 space-x-3 rounded-md bg-[#F2F6FF]">
+      <section className="flex items-center justify-center mx-auto text-center max-w-7xl">
+        <img src={party} />
         <div>
-          <h1 className="font-medium text-[#004182] text-3xl md:text-5xl tracking-wide">
+          <h1 className="mb-3 text-4xl font-semibold text-red-500">
             Leaderboard
           </h1>
-          {/* <p className="text-sm md:text-lg mt-2 tracking-wide">
-            Potential to earn Up to $10 Million Commission (You wish)
-          </p> */}
+          <p className="mb-3 text-lg">
+            See who has earned the most from referrals!
+          </p>
         </div>
-
-        <img className="size-24" src={board} alt="Board" />
+        <img src={party} className="scale-x-[-1]" />
       </section>
 
       <section className="flow-root mt-8">
         <div className="overflow-x-auto">
-          <div className="inline-block min-w-full py-2 align-middle px-1">
-            <div className="overflow-hidden shadow-sm ring-1 ring-black/5 rounded-lg">
+          <div className="inline-block min-w-full px-1 py-2 align-middle">
+            <div className="overflow-hidden rounded-lg shadow-sm ring-1 ring-black/5">
               <table className="min-w-full divide-y divide-gray-300">
-                <thead className="bg-white">
-                  <tr>
-                    <th
-                      scope="col"
-                      className="py-3.5 pr-3 pl-4 text-left text-sm font-bold text-gray-900 sm:pl-6"
-                    >
-                      Ranking
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-3 py-3.5 text-left text-sm font-bold text-gray-900"
-                    >
-                      Members
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-3 py-3.5 text-left text-sm font-bold text-gray-900"
-                    >
-                      Total Earning
-                    </th>
-                  </tr>
-                </thead>
                 <tbody className="bg-white">
                   {people.map((person, i) => (
                     <tr key={i} className="even:bg-gray-50">
                       <td className="py-4 pl-4 pr-3 text-sm font-medium text-gray-900 whitespace-nowrap sm:pl-6">
-                        <p
-                          className={clsx(
-                            i === 0 ? "text-[#004182] font-semibold text-xl" : ""
-                          )}
-                        >
-                          {i + 1}
+                        <p>
+                          <span className="p-3 text-lg text-blue-700 bg-gray-200 border rounded-full border-amber-400">
+                            #{i + 1}
+                          </span>
                         </p>
                       </td>
                       <td className="px-3 py-4 text-sm whitespace-nowrap">
@@ -98,13 +73,7 @@ export default function CardinalLeaderboard() {
                         </div>
                       </td>
                       <td className="px-3 py-4 text-sm whitespace-nowrap">
-                        <p
-                          className={clsx(
-                            i === 0 ? "text-[#004182] text-xl font-semibold" : ""
-                          )}
-                        >
-                          ${person.earning}
-                        </p>
+                        <p>${person.earning}</p>
                       </td>
                     </tr>
                   ))}
